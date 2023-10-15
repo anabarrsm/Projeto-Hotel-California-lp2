8dc1a42b304f016cd99a3d41aa81abc2ebb1ce84
Lucasppalmeira <jose.lucas.silva.palmeira@ccc.ufcg.edu.br>
2023-10-15 14:08:19 -0300

    Documentando alguns metodos do Controller de Refeicao

--- src/br/edu/ufcg/computacao/p2lp2/hotelcalifornia/RefeicaoController.java
+++ src/br/edu/ufcg/computacao/p2lp2/hotelcalifornia/RefeicaoController.java
@@ -11,6 +11,17 @@ import java.util.ArrayList;
 public class RefeicaoController {
     private ArrayList<Refeicao> refeicoes = new ArrayList<>();
 
+    /**
+     * Cria uma nova refeição e adiciona no ArrayList de refeições.
+     * @param idAutenticacao / id da refeição.
+     * @param tipoRefeicao / tipo da refeição(café da manhã, almoço, janta).
+     * @param titulo / titulo ou descrição da refeição.
+     * @param horarioInicio / hora de inicio.
+     * @param horarioFinal / hora de fim.
+     * @param valor / valor da refeição.
+     * @param disponivel / disponibilidade da refeição.
+     * @return
+     */
     public String disponibilizarRefeicao(String idAutenticacao, String tipoRefeicao, String titulo, LocalTime horarioInicio, LocalTime horarioFinal, double valor, boolean disponivel) {
         long id = Long.parseLong(idAutenticacao);
         Refeicao r = new Refeicao(id, tipoRefeicao, titulo, horarioInicio, horarioFinal, valor, disponivel);
@@ -18,6 +29,14 @@ public class RefeicaoController {
         return "Refeição adicionada!";
     }
 
+    /**
+     * Altera os valores que podem ser alterados em uma refeição.
+     * @param idRefeicao / id da refeição a ser alterada.
+     * @param horarioInicio / novo horario de inicio.
+     * @param horarioFinal / novo horario de fim.
+     * @param disponivel / disponibilidade da refeição.
+     * @return
+     */
     public String alterarRefeicao(long idRefeicao, LocalTime horarioInicio, LocalTime horarioFinal, boolean disponivel) {
         for (Refeicao r: refeicoes) {
             if(r.getId() == idRefeicao){
